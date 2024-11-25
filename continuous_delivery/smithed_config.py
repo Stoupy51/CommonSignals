eff87b94627e2d7f1e8ac2932748b1dc148ca221
Stoupy51 <stoupy51@gmail.com>
2024-11-25 22:41:42 +0100

    build: 🚀 Fixed smithed upload config

--- continuous_delivery/smithed_config.py
+++ continuous_delivery/smithed_config.py
@@ -4,8 +4,8 @@ from config import *
 
 # Configuration
 smithed_config: dict = {
-	"project_id": PROJECT_NAME.replace(" ", ""),
-	"project_name": PROJECT_NAME,
+	"project_id": NAMESPACE,
+	"project_name": PROJECT_NAME.replace(" ", ""),
 	"version": VERSION,
 }
 
