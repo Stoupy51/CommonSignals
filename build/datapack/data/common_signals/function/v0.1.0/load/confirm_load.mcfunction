a879cde2e0437e20f4d7967adbb3515d22f3d2f9
Stoupy51 <stoupy51@gmail.com>
2025-07-01 12:25:08 +0200

    build: 🚀 Now using StewBeet

--- build/datapack/data/common_signals/function/v0.1.0/load/confirm_load.mcfunction
+++ build/datapack/data/common_signals/function/v0.1.0/load/confirm_load.mcfunction
@@ -4,7 +4,7 @@
 # @within	common_signals:v0.1.0/load/secondary
 #
 
+# Confirm load
 tellraw @a[tag=convention.debug] {"text":"[Loaded Common Signals v0.1.0]","color":"green"}
-
 scoreboard players set #common_signals.loaded load.status 1
 
--- user/link.py
+++ user/link.py
@@ -1,52 +1,141 @@
 
 # Imports
-from python_datapack.constants import *
-from python_datapack.utils.print import *
-from python_datapack.utils.io import *
+from stewbeet import Advancement, Context, FunctionTag, LootTable, set_json_encoder, write_tick_file, write_versioned_function
+
 
 # Main function is run just before making finalyzing the build process (zip, headers, lang, ...)
-def main(config: dict) -> None:
-	database: dict[str, dict] = config["database"]
-	namespace: str = config["namespace"]
-	version: str = config["version"]
+def beet_default(ctx: Context) -> None:
+	ns: str = ctx.project_id
+	version: str = ctx.project_version
 	major, minor, patch = version.split(".")
-	functions: str = f"{config['datapack_functions']}/v{version}"
-	advancements: str = f"{config['build_datapack']}/data/{namespace}/advancement/v{version}"
-	function_tags: str = f"{config['build_datapack']}/data/{namespace}/tags/function"
 
 	# Write tick
-	write_to_file(f"{functions}/tick.mcfunction", f"""
+	write_tick_file(f"""
 # For each new item discovered, run a signal
-execute as @e[type=item,tag=!{namespace}.checked] run function {namespace}:v{version}/technical/on_new_item
+execute as @e[type=item,tag=!{ns}.checked] run function {ns}:v{version}/technical/on_new_item
 """)
-	
+
 
 	# Write inventory changed advancement functions
-	write_to_file(f"{functions}/inventory_changed.mcfunction", f"""
-advancement revoke @s only {namespace}:v{version}/inventory_changed
-execute if score #{namespace}.major load.status matches {major} if score #{namespace}.minor load.status matches {minor} if score #{namespace}.patch load.status matches {patch} run function {namespace}:v{version}/technical/inventory_changed
+	write_versioned_function("inventory_changed", f"""
+advancement revoke @s only {ns}:v{version}/inventory_changed
+execute if score #{ns}.major load.status matches {major} if score #{ns}.minor load.status matches {minor} if score #{ns}.patch load.status matches {patch} run function {ns}:v{version}/technical/inventory_changed
 """)
-	write_to_file(f"{functions}/technical/inventory_changed.mcfunction", f'clear @s *[minecraft:custom_data={{"{namespace}":{{"temp":true}}}}]')
+	write_versioned_function("technical/inventory_changed", f'clear @s *[minecraft:custom_data={{"{ns}":{{"temp":true}}}}]')
 
 	# Write inventory changed advancement
-	adv: dict = {"criteria": {"requirement": {"trigger": "minecraft:inventory_changed"}},"rewards": {"function": f"{namespace}:v{version}/inventory_changed"}}
-	write_to_file(f"{advancements}/inventory_changed.json", super_json_dump(adv, max_level = -1))
+	adv: dict = {"criteria": {"requirement": {"trigger": "minecraft:inventory_changed"}},"rewards": {"function": f"{ns}:v{version}/inventory_changed"}}
+	ctx.data[ns].advancements[f"v{version}/inventory_changed"] = set_json_encoder(Advancement(adv), max_level=-1)
 
 	# Write on_new_item function and function tag
-	write_to_file(f"{functions}/technical/on_new_item.mcfunction", f"""
-tag @s add {namespace}.checked
-function #{namespace}:signals/on_new_item
+	write_versioned_function("technical/on_new_item", f"""
+tag @s add {ns}.checked
+function #{ns}:signals/on_new_item
 
 # Check if the item is a temporary item and kill it
-execute if data entity @s Item.components."minecraft:custom_data".{namespace}.temp run kill @s
+execute if data entity @s Item.components."minecraft:custom_data".{ns}.temp run kill @s
 """)
-	write_to_file(f"{function_tags}/signals/on_new_item.json", super_json_dump({"values": []}))
+	ctx.data[ns].function_tags["signals/on_new_item"] = set_json_encoder(FunctionTag({"values": []}))
 
 	## Write loot tables for polished deepslate
-	vanilla_polished_deepslate: dict = {"type":"minecraft:block","pools":[{"rolls":1,"entries":[{"type":"minecraft:loot_table","value":f"{namespace}:polished_deepslate"}]},{"rolls":1,"bonus_rolls":0,"entries":[{"type":"minecraft:item","name":"minecraft:polished_deepslate"}],"conditions":[{"condition":"minecraft:survives_explosion"}]}],"random_sequence":"minecraft:blocks/polished_deepslate","__smithed__":{"rules":[{"type":"smithed:append","target":"pools[0].entries","source":{"type":"smithed:reference","path":"pools[0].entries[0]"}}]}}
-	namespace_polished_deepslate: dict = {"type":"minecraft:block","pools":[{"rolls":1,"entries":[{"type":"minecraft:item","name":"minecraft:deepslate","functions":[{"function":"minecraft:set_components","components":{"minecraft:item_model":"air","minecraft:custom_data":f"{{\"{namespace}\":{{\"temp\":true}}}}"}},{"function":"minecraft:set_components","components":{"minecraft:custom_data":f"{{\"{namespace}\":{{\"silk_touch\":true,\"temp\":true}}}}"},"conditions":[{"condition":"minecraft:match_tool","predicate":{"predicates":{"minecraft:enchantments":[{"enchantments":"minecraft:silk_touch","levels":{"min":1}}]}}}]},{"function":"minecraft:apply_bonus","enchantment":"minecraft:fortune","formula":"minecraft:ore_drops"}]}],"conditions":[{"condition":"minecraft:survives_explosion"}]}]}
-	write_to_file(f"{config['build_datapack']}/data/{namespace}/loot_table/polished_deepslate.json", super_json_dump(namespace_polished_deepslate, max_level = -1))
-	write_to_file(f"{config['build_datapack']}/data/minecraft/loot_table/blocks/polished_deepslate.json", super_json_dump(vanilla_polished_deepslate, max_level = -1))
+	ctx.data[ns].loot_tables["polished_deepslate"] = set_json_encoder(LootTable({
+		"type": "minecraft:block",
+		"pools": [
+			{
+				"rolls": 1,
+				"entries": [
+					{
+						"type": "minecraft:item",
+						"name": "minecraft:deepslate",
+						"functions": [
+							{
+								"function": "minecraft:set_components",
+								"components": {
+									"minecraft:item_model": "air",
+									"minecraft:custom_data": f"""{{"{ns}":{{"temp":true}}}}"""
+								}
+							},
+							{
+								"function": "minecraft:set_components",
+								"components": {
+									"minecraft:custom_data": f"""{{"{ns}":{{"silk_touch":true,"temp":true}}}}"""
+								},
+								"conditions": [
+									{
+										"condition": "minecraft:match_tool",
+										"predicate": {
+											"predicates": {
+												"minecraft:enchantments": [
+													{
+														"enchantments": "minecraft:silk_touch",
+														"levels": {
+															"min": 1
+														}
+													}
+												]
+											}
+										}
+									}
+								]
+							},
+							{
+								"function": "minecraft:apply_bonus",
+								"enchantment": "minecraft:fortune",
+								"formula": "minecraft:ore_drops"
+							}
+						]
+					}
+				],
+				"conditions": [
+					{
+						"condition": "minecraft:survives_explosion"
+					}
+				]
+			}
+		]
+	}), max_level=7)
+	ctx.data["minecraft"].loot_tables["blocks/polished_deepslate"] = set_json_encoder(LootTable({
+		"type": "minecraft:block",
+		"pools": [
+			{
+				"rolls": 1,
+				"entries": [
+					{
+						"type": "minecraft:loot_table",
+						"value": "common_signals:polished_deepslate"
+					}
+				]
+			},
+			{
+				"rolls": 1,
+				"bonus_rolls": 0,
+				"entries": [
+					{
+						"type": "minecraft:item",
+						"name": "minecraft:polished_deepslate"
+					}
+				],
+				"conditions": [
+					{
+						"condition": "minecraft:survives_explosion"
+					}
+				]
+			}
+		],
+		"random_sequence": "minecraft:blocks/polished_deepslate",
+		"__smithed__": {
+			"rules": [
+				{
+					"type": "smithed:append",
+					"target": "pools[0].entries",
+					"source": {
+						"type": "smithed:reference",
+						"path": "pools[0].entries[0]"
+					}
+				}
+			]
+		}
+	}), max_level=7)
 
 	pass
 
